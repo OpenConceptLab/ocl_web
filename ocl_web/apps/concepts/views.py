90eb58aeda2bfa54cacbfb7fada07a89207bfa72
Brian Dant <briandant414@gmail.com>
2014-03-23 20:26:11 -0400

    Initial commit for concept details.

--- ocl_web/apps/concepts/views.py
+++ ocl_web/apps/concepts/views.py
@@ -4,4 +4,23 @@ import requests
 
 
 class ConceptDetailView(TemplateView):
-    pass
+    template_name = "concepts/concept.html"
+
+    def get_context_data(self, *args, **kwargs):
+
+        context = super(ConceptDetailView, self).get_context_data(*args, **kwargs)
+
+        host = settings.API_HOST
+        auth_token = settings.API_TOKEN
+        full_path = host + uri_path
+        headers = {'Authorization': auth_token}
+
+        results = requests.get(full_path, headers=headers)
+        context['results'] = results.json()
+
+        import pdb; pdb.set_trace()
+        return context
+
+
+
+
--- ocl_web/apps/orgs/urls.py
+++ ocl_web/apps/orgs/urls.py
@@ -7,6 +7,6 @@ urlpatterns = patterns('',
 #    url(r'^(?P<org>[a-zA-Z0-9\-\.]+)/$', OrganizationDetailView.as_view(), name='organization-detail'),
 #    url(r'^(?P<org>[a-zA-Z0-9\-\.]+)/members/$', UserListView.as_view(), {'related_object_type': Organization, 'related_object_kwarg': 'org', 'related_object_attribute': 'members'}, name='organization-members'),
 #    url(r'^(?P<org>[a-zA-Z0-9\-\.]+)/members/(?P<user>[a-zA-Z0-9\-]+)/$', OrganizationMemberView.as_view(), name='organization-member-detail'),
-    url(r'^(?P<org>[a-zA-Z0-9\-\.]+)/sources/', include('apps.sources.urls')),
+    url(r'^(?P<owner>[a-zA-Z0-9\-\.]+)/sources/', include('apps.sources.urls')),
 #    url(r'^(?P<org>[a-zA-Z0-9\-\.]+)/collections/', include('conceptcollections.urls')),
 )
