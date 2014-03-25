0e9773366c852a2699588e6a98d7635216eb6775
Jonathan Payne <paynejd@gmail.com>
2014-03-25 10:46:26 -0400

    Updated concept details page and fake data

--- ocl_web/apps/concepts/views.py
+++ ocl_web/apps/concepts/views.py
@@ -19,7 +19,36 @@ class ConceptDetailView(TemplateView):
             ownerType="Organization",
             displayName="Tuberculosis of lung, confirmed by culture only",
             displayLocale="en",
+            versioned_object_url="http://65.99.230.144/v1/orgs/WHO/sources/ICD-10/concepts/A15.1/",
             url="http://65.99.230.144/v1/orgs/WHO/sources/ICD-10/concepts/A15.1/52957df350d61b2b63e18f88/",
+            descriptions=[
+                dict(
+                    locale="en",
+                    type=None,
+                    name="Tuberculous bronchiectasis, fibrosis of lung, pneumonia, pneumothorax, confirmed by sputum microscopy with culture only",
+                    localePreferred=False
+                ),
+                dict(
+                    locale="fr",
+                    type=None,
+                    name="French definition!",
+                    localePreferred=False
+                )
+            ],
+            names=[
+                dict(
+                    locale="en",
+                    type=None,
+                    name="Tuberculosis of lung, confirmed by culture only",
+                    localePreferred=True
+                ),
+                dict(
+                    locale="fr",
+                    type=None,
+                    name="Tuberculose pulmonaire, confirmee par culture seulement",
+                    localePreferred=False
+                )
+            ]
         )
 
         context['concept'] = fake_concept
