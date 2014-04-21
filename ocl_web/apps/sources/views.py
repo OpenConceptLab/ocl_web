a488faa4218c436cd719415e3603f32cd73664f9
Jonathan Payne <paynejd@gmail.com>
2014-04-21 09:24:10 -0400

    Getting source details up to speed

--- ocl_web/apps/sources/views.py
+++ ocl_web/apps/sources/views.py
@@ -11,6 +11,7 @@ class SourceDetailView(TemplateView):
 
         context = super(SourceDetailView, self).get_context_data(*args, **kwargs)
 
+        # Setup API calls
         host = settings.API_HOST
         auth_token = settings.API_TOKEN
         source_path = "/v1/orgs/%s/sources/%s/" % (kwargs['org'], kwargs['source'])
@@ -19,11 +20,22 @@ class SourceDetailView(TemplateView):
         concept_url = host + concept_path
         headers = {'Authorization': auth_token}
 
+        # API calls for source details and concepts
         source = requests.get(source_url, headers=headers).json()
         concepts = requests.get(concept_url, headers=headers).json()
 
+        # Get additional details for concepts (since currently not included in list query)
+        concepts_detail = []
+        for concept in concepts:
+            # this is a total hack to address a bug in how the API creates URLs
+            concept_url = concept['url'].replace('INITIAL/','')
+            # this part is a hack since the API doesn't let me customize the fields returned
+            concept = requests.get(concept_url, headers=headers).json()
+            concepts_detail.append(concept)
+
         context['source'] = source
-        context['concepts'] = concepts
+        #context['concepts'] = concepts
+        context['concepts'] = concepts_detail
 
         return context
 
