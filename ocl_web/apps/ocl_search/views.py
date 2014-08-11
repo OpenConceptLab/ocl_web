0ea4fc8aecf64f92c8a396f3fdde0e0fa3a8913f
Jonathan Payne <paynejd@gmail.com>
2014-08-11 10:46:57 -0400

    Added resource counts and pagination to Search

--- ocl_web/apps/ocl_search/views.py
+++ ocl_web/apps/ocl_search/views.py
@@ -8,6 +8,8 @@
 from django.views.generic import TemplateView
 from django.conf import settings
 import requests
+import urllib
+import math
 
 
 class HomeSearchView(TemplateView):
@@ -19,7 +21,7 @@ class HomeSearchView(TemplateView):
         context = super(HomeSearchView, self).get_context_data(*args, **kwargs)
 
         # Resolves search type to the API path
-        SEARCH_TYPE_PATHS = {
+        search_type_paths = {
             'concepts': '/v1/concepts/',
             'sources': '/v1/sources/',
             'collections': '/v1/collections/',
@@ -28,7 +30,7 @@ class HomeSearchView(TemplateView):
         }
 
         # Resolves search type to the English singular word form
-        searchTypeNames = {
+        search_type_names = {
             'concepts': 'concept',
             'sources': 'source',
             'collections': 'collection',
@@ -36,46 +38,101 @@ class HomeSearchView(TemplateView):
             'users': 'user'
         }
 
-        # Default search type to 'concepts' if not valid
-        try:
-            uri_path = SEARCH_TYPE_PATHS[self.request.GET['type']]
-            searchType = self.request.GET['type']
-        except KeyError:  # Either there is no 'type' in GET, or we don't support what was sent in.
-            uri_path = SEARCH_TYPE_PATHS['concepts']
-            searchType = 'concepts'
+        # Default search type to 'concepts' if missing or invalid
+        if 'type' in self.request.GET and self.request.GET['type'] in search_type_names:
+            search_type = self.request.GET['type']
+        else:
+            search_type = 'concepts'
 
-        # Setup the primary search
+        # Grab API settings
         host = settings.API_HOST
         auth_token = settings.API_TOKEN
-        full_path = host + uri_path
-        requestHeaders = {'Authorization': auth_token}
+        num_per_page = 1
+
+        # Setup the primary search
+        search_params = {}
+        search_params['limit'] = num_per_page
+        if 'q' in self.request.GET:
+            search_params['q'] = self.request.GET['q']
+        # HACK: This is a hack until the verbose concept bug is fixed
+        if search_type != 'concepts':
+            search_params['verbose'] = 'true'
+        search_url = host + search_type_paths[search_type]
+        if search_params:
+            search_url = search_url + '?' + urllib.urlencode(search_params)
+        request_headers = {'Authorization': auth_token}
 
         # Perform the primary search using the API
         try:
-            response = requests.get(full_path, headers=requestHeaders)
+            response = requests.get(url=search_url, headers=request_headers)
             results = response.json()
-            responseHeaders = response.headers
+            response_headers = response.headers
         except:
             response = None
             results = []
-            responseHeaders = None
+            response_headers = None
 
-        # Load full resource details (since many required fields are not currently included in the list query)
-        # NOTE: This is a temporary fix until the API supports field selection as a GET parameter
-        if (searchType in ['sources', 'collections', 'orgs', 'users']):
-            results_detail = []
-            for result_summary in results:
+        # Setup pagination
+        resource_count = {}
+        if response:
+            num_found = resource_count[search_type] = int(response_headers['num_found'])
+            num_returned = int(response_headers['num_returned'])
+            offset = int(response_headers['offset'])
+            results_display_start = offset + 1
+            results_display_stop = results_display_start + num_returned - 1
+            num_pages = int(math.ceil(float(num_found) / num_per_page))
+            current_page = int(math.ceil(float(offset + 1) / num_per_page))
+
+            # HACK: Setting up paginator
+            pages = []
+            x = 0
+            while x < min(num_pages, 2):
+                x = x + 1
+                pages.append(x)
+            if current_page > 3:
+                pages.append('...')
+            if current_page > 2:
+                pages.append(current_page)
+            if num_pages > 2 and current_page < (num_pages - 2):
+                pages.append('...')
+            if num_pages > 2 and current_page < (num_pages - 1):
+                pages.append(num_pages - 1)
+            if num_pages > 2 and current_page < num_pages:
+                pages.append(num_pages)
+
+        else:
+            resource_count[search_type] = -1
+            results_display_start = results_display_stop = 0
+            num_pages = current_page = 0
+        pagination = {
+            'start': results_display_start,
+            'stop': results_display_stop,
+            'currentPage': current_page,
+            'totalPages': num_pages,
+            'pages': pages,
+            'urlNext': '',
+            'urlPrevious': ''
+        }
+
+        # Perform the counter searches
+        if response:
+            for resource_type in search_type_names:
+                if resource_type == search_type:
+                    continue
+                counter_search_url = host + search_type_paths[resource_type]
                 try:
-                    result_detail = requests.get(result_summary['url'], headers=requestHeaders).json()
-                    results_detail.append(result_detail)
+                    count_response = requests.head(url=counter_search_url, headers=request_headers)
+                    resource_count[resource_type] = int(count_response.headers['num_found'])
                 except:
-                    results_detail.append(result_summary)
-            results = results_detail
+                    resource_count[resource_type] = -1
 
         # Add data to the context
+        context['apiSearchQuery'] = search_url
         context['results'] = results
-        context['searchType'] = searchType
-        context['searchTypeName'] = searchTypeNames[searchType]
-        context['responseHeaders'] = responseHeaders
+        context['searchType'] = search_type
+        context['searchTypeName'] = search_type_names[search_type]
+        context['responseHeaders'] = response_headers
+        context['resourceCount'] = resource_count
+        context['pagination'] = pagination
 
         return context
