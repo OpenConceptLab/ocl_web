906fe9d812fec22b25aeb8ede112ad5c4637d155
Jonathan Payne <paynejd@gmail.com>
2014-08-16 15:36:14 -0400

    Implemented prev/next search page buttons

--- ocl_web/apps/ocl_search/views.py
+++ ocl_web/apps/ocl_search/views.py
@@ -20,6 +20,13 @@ class HomeSearchView(TemplateView):
 
         context = super(HomeSearchView, self).get_context_data(*args, **kwargs)
 
+        # Defaults
+        default_search_type = 'concepts'
+        default_num_per_page = 2
+        host = settings.API_HOST
+        auth_token = settings.API_TOKEN
+        page_url = '/search/'
+
         # Resolves search type to the API path
         search_type_paths = {
             'concepts': '/v1/concepts/',
@@ -38,104 +45,127 @@ class HomeSearchView(TemplateView):
             'users': 'user'
         }
 
-        # Default search type to 'concepts' if missing or invalid
+        # Setup the resource count dictionary        
+        resource_count = {}
+        for resource_type in search_type_names:
+            resource_count[resource_type] = 0
+
+        # Default search type if missing or invalid
         if 'type' in self.request.GET and self.request.GET['type'] in search_type_names:
             search_type = self.request.GET['type']
         else:
-            search_type = 'concepts'
+            search_type = default_search_type
 
-        # Grab API settings
-        host = settings.API_HOST
-        auth_token = settings.API_TOKEN
-        num_per_page = 2
-
-        # Setup the primary search
-        search_params = {}
-        search_params['limit'] = num_per_page
-        search_params['verbose'] = 'true'
+        # Set parameters for primary search
+        search_params = {
+            'verbose': 'true'
+        }
         if 'q' in self.request.GET:
             search_params['q'] = self.request.GET['q']
-        search_url = host + search_type_paths[search_type]
         if 'page' in self.request.GET:
             try:
-                search_params['page'] = int(self.request.GET['page'])
+                search_params['page'] = current_page = int(self.request.GET['page'])
             except:
-                pass
+                current_page = 1
+        if 'limit' in self.request.GET:
+            try:
+                search_params['limit'] = num_per_page = int(self.request.GET['limit'])
+            except:
+                search_params['limit'] = num_per_page = default_num_per_page
+        else:
+            search_params['limit'] = num_per_page = default_num_per_page
+
+        # Setup API request headers
+        search_request_headers = {'Authorization': auth_token}
+
+        # Setup primary search API URL
+        search_url = host + search_type_paths[search_type]
         if search_params:
             search_url = search_url + '?' + urllib.urlencode(search_params)
-        request_headers = {'Authorization': auth_token}
 
-        # Perform the primary search using the API
+        # Perform the primary search via the API
+        # TODO: Improve the handling of search errors
         try:
-            response = requests.get(url=search_url, headers=request_headers)
-            results = response.json()
-            response_headers = response.headers
+            search_response = requests.get(url=search_url, headers=search_request_headers)
+            search_results = search_response.json()
+            search_response_headers = search_response.headers
         except:
-            response = None
-            results = []
-            response_headers = None
+            search_response = None
+            search_results = []
+            search_response_headers = None
 
         # Setup pagination
-        resource_count = {}
-        if response:
-            num_found = resource_count[search_type] = int(response_headers['num_found'])
-            num_returned = int(response_headers['num_returned'])
-            offset = int(response_headers['offset'])
-            results_display_start = offset + 1
-            results_display_stop = results_display_start + num_returned - 1
-            num_pages = int(math.ceil(float(num_found) / num_per_page))
-            current_page = int(math.ceil(float(offset + 1) / num_per_page))
+        paginator_bar = []
+        results_display_start = results_display_stop = 0
+        num_pages = current_page = 0
+        page_url_prev = ''
+        page_url_next = ''
+        if search_response:
+            num_found = resource_count[search_type] = int(search_response_headers['num_found'])
+            num_returned = int(search_response_headers['num_returned'])
+            offset = int(search_response_headers['offset'])  # 0-index
+            results_display_start = offset + 1  # 1-index
+            results_display_stop = results_display_start + num_returned - 1  # 1-index
+            num_pages = int(math.ceil(float(num_found) / num_per_page))  # 1-index
+            current_page = int(math.ceil(float(offset + 1) / num_per_page))  # 1-index
 
-            # HACK: Setting up paginator
-            pages = []
+            # Build previous page url
+            if current_page > 1:
+                page_params_prev = self.request.GET.copy()
+                page_params_prev['page'] = current_page - 1
+                page_url_prev = page_url + '?' + urllib.urlencode(page_params_prev)
+
+            # Build next page url
+            if current_page < num_pages:
+                page_params_next = self.request.GET.copy()
+                page_params_next['page'] = current_page + 1
+                page_url_next = page_url + '?' + urllib.urlencode(page_params_next)
+
+            # HACK: Setup paginator bar
             x = 0
             while x < min(num_pages, 2):
                 x = x + 1
-                pages.append(x)
+                paginator_bar.append(x)
             if current_page > 3:
-                pages.append('...')
+                paginator_bar.append('...')
             if current_page > 2:
-                pages.append(current_page)
+                paginator_bar.append(current_page)
             if num_pages > 2 and current_page < (num_pages - 2):
-                pages.append('...')
+                paginator_bar.append('...')
             if num_pages > 2 and current_page < (num_pages - 1):
-                pages.append(num_pages - 1)
+                paginator_bar.append(num_pages - 1)
             if num_pages > 2 and current_page < num_pages:
-                pages.append(num_pages)
+                paginator_bar.append(num_pages)
 
-        else:
-            resource_count[search_type] = -1
-            results_display_start = results_display_stop = 0
-            num_pages = current_page = 0
         pagination = {
-            'start': results_display_start,
-            'stop': results_display_stop,
-            'currentPage': current_page,
-            'totalPages': num_pages,
-            'pages': pages,
-            'urlNext': '',
-            'urlPrevious': ''
+            'results_start_index': results_display_start,
+            'results_stop_index': results_display_stop,
+            'current_page': current_page,
+            'total_pages': num_pages,
+            'paginator_bar': paginator_bar,
+            'search_url_next_page': page_url_next,
+            'search_url_prev_page': page_url_prev
         }
 
         # Perform the counter searches
-        if response:
+        if search_response:
             for resource_type in search_type_names:
                 if resource_type == search_type:
                     continue
                 counter_search_url = host + search_type_paths[resource_type]
                 try:
-                    count_response = requests.head(url=counter_search_url, headers=request_headers)
+                    count_response = requests.head(url=counter_search_url, headers=search_request_headers)
                     resource_count[resource_type] = int(count_response.headers['num_found'])
                 except:
-                    resource_count[resource_type] = -1
+                    resource_count[resource_type] = 0
 
         # Add data to the context
-        context['apiSearchQuery'] = search_url
-        context['results'] = results
-        context['searchType'] = search_type
-        context['searchTypeName'] = search_type_names[search_type]
-        context['responseHeaders'] = response_headers
-        context['resourceCount'] = resource_count
+        context['api_search_query'] = search_url
+        context['results'] = search_results
+        context['search_type'] = search_type
+        context['search_type_name'] = search_type_names[search_type]
+        context['search_response_headers'] = search_response_headers
+        context['resource_count'] = resource_count
         context['pagination'] = pagination
 
         return context
