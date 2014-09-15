a570829c584c7e701838a2fe1392f05f937142e1
Jonathan Payne <paynejd@gmail.com>
2014-09-15 14:32:41 -0400

    Updated orgs to new API json

--- ocl_web/apps/orgs/views.py
+++ ocl_web/apps/orgs/views.py
@@ -39,31 +39,22 @@ class OrganizationDetailView(TemplateView):
         org = requests.get(org_url, headers=requestHeaders).json()
 
         # Get sources owned by the org
-        sources_url = org['sources_url']
+        sources_url = org['sources_url'] + '?verbose=true'
         sources = requests.get(sources_url, headers=requestHeaders).json()
 
         # Get collections owned by the org
         collections_path = "/v1/orgs/%s/collections/" % kwargs['org']  # The org object should have this in the future.
-        collections_url = host + collections_path
+        collections_url = host + collections_path + '?verbose=true'
         collections = requests.get(collections_url, headers=requestHeaders).json()
 
         # Get members of the org
         members_url = org['members_url']
         members = requests.get(members_url, headers=requestHeaders).json()
 
-        # Get additional details for sources and collections (since currently not included in the list query)
-        sources_detail = []
-        for source in sources:
-            source = requests.get(source['url'], headers=requestHeaders).json()
-            sources_detail.append(source)
-        collections_detail = []
-        for collection in collections:
-            collection = requests.get(collection['url'], headers=requestHeaders).json()
-            collections_detail.append(collection)
-
+        # Set the context
         context['org'] = org
-        context['sources'] = sources_detail
-        context['collections'] = collections_detail
+        context['sources'] = sources
+        context['collections'] = collections
         context['members'] = members
 
         return context
