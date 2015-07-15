59eda184e5676221f65c04817b034cba854feab5
Jonathan Payne <paynejd@gmail.com>
2015-07-15 13:42:53 -0400

    Applying fix to source url config file

--- ocl_web/apps/sources/urls.py
+++ ocl_web/apps/sources/urls.py
@@ -3,13 +3,13 @@ from .views import SourceDetailView, SourceCreateView
 
 # TODO(paynejd@gmail.com): I believe this is file retired...
 
-#urlpatterns = patterns('',
+urlpatterns = patterns('',
 
 	# TODO(paynejd@gmail.com): Retire? If being used, move into the main URL config files
-    #url(r'^detail/(?P<org>[a-zA-Z0-9\-\.]+)/(?P<source>[a-zA-Z0-9\-\.]+)/$',
-    #	SourceDetailView.as_view(), name='source-detail'),
-    #url(r'^create/(?P<org>[a-zA-Z0-9\-\.]+)/$',
-    #	SourceCreateView.as_view(), name='source-create-for-org'),
+    url(r'^detail/(?P<org>[a-zA-Z0-9\-\.]+)/(?P<source>[a-zA-Z0-9\-\.]+)/$',
+    	SourceDetailView.as_view(), name='source-detail'),
+    url(r'^create/(?P<org>[a-zA-Z0-9\-\.]+)/$',
+    	SourceCreateView.as_view(), name='source-create-for-org'),
 
     # RETIRED
 	#url(r'^$', SourceListView.as_view(), name='source-list'),
@@ -18,4 +18,4 @@ from .views import SourceDetailView, SourceCreateView
 	#url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/$', SourceVersionRetrieveUpdateDestroyView.as_view(), name='sourceversion-detail'),
 	#url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/children/$', SourceVersionChildListView.as_view(), {'list_children': True}, name='sourceversion-child-list'),
 	#url(r'^(?P<source>[a-zA-Z0-9\-\.]+)/(?P<version>[a-zA-Z0-9\-\.]+)/concepts/', include('apps.concepts.urls'))
-#)
+)
