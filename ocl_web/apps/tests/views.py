0bdf7a99cb1b34938bf95de967e17712964c3ee9
Jonathan Payne <paynejd@gmail.com>
2015-08-03 10:20:54 -0400

    Removed dummy code from TestTagsView

--- ocl_web/apps/tests/views.py
+++ ocl_web/apps/tests/views.py
@@ -1,16 +1,10 @@
 """
 Test views
 """
-import requests
-
-from django.views.generic import (TemplateView, View)
+from django.views.generic import TemplateView
 
 class TestTagsView(TemplateView):
-	"""
+    """
     View to test resource tags
     """
     template_name = "tests/tags.html"
-
-    def get_context_data(self, *args, **kwargs):
-    	""" Do nothing for now """
-    	return {}
