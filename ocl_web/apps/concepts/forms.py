7207f044280413940d43d18d83ee893be940255a
Jonathan Payne <paynejd@gmail.com>
2015-08-06 14:38:30 -0400

    Implemented concept edit form

--- ocl_web/apps/concepts/forms.py
+++ ocl_web/apps/concepts/forms.py
@@ -94,64 +94,70 @@ class ConceptNewForm(forms.Form):
 
 
 
-class ConceptCreateForm(forms.Form):
+# TODO: Retire this
+# class ConceptCreateForm(forms.Form):
+#     """
+#         Concept create form
+#     """
+#     required_css_class = 'required'
+
+#     concept_id = forms.CharField(
+#         label=_('Concept ID'), 
+#         max_length=256, 
+#         required=True)
+#     concept_class = forms.CharField(
+#         label=_('Concept Class'), 
+#         required=True)
+#     datatype = forms.CharField(
+#         label=_('Datatype'), 
+#         required=False)
+#     name = forms.CharField(
+#         label=_('Name'),
+#         max_length=256,
+#         required=True)
+#     locale = forms.ChoiceField(
+#         label=_('Name Locale'), 
+#         choices=[(d['code'], d['name']) for d in _get_locale_list()], 
+#         required=True)
+#     preferred_locale = forms.BooleanField(
+#         label=_('Preferred Locale'), 
+#         required=False, 
+#         initial=False)
+
+#     def clean_concept_id(self):
+#         """ concept ID must be unique """
+#         concept_id = self.cleaned_data['concept_id']
+#         source = self.initial['source']
+#         request = self.initial['request']
+#         api = OCLapi(request, debug=True)
+#         result = api.get('orgs', source['owner'], 'sources', source['id'], 'concepts', concept_id)
+#         if result.status_code == 200:
+#             raise forms.ValidationError(_('This Concept ID is already used in this source.'))
+#         return concept_id
+
+
+
+class ConceptEditForm(ConceptNewForm):
     """
-        Concept create form
-    """
-    required_css_class = 'required'
-
-    concept_id = forms.CharField(
-        label=_('Concept ID'), 
-        max_length=256, 
-        required=True)
-    concept_class = forms.CharField(
-        label=_('Concept Class'), 
-        required=True)
-    datatype = forms.CharField(
-        label=_('Datatype'), 
-        required=False)
-    name = forms.CharField(
-        label=_('Name'),
-        max_length=256,
-        required=True)
-    locale = forms.ChoiceField(
-        label=_('Name Locale'), 
-        choices=[(d['code'], d['name']) for d in _get_locale_list()], 
-        required=True)
-    preferred_locale = forms.BooleanField(
-        label=_('Preferred Locale'), 
-        required=False, 
-        initial=False)
-
-    def clean_concept_id(self):
-        """ concept ID must be unique """
-        concept_id = self.cleaned_data['concept_id']
-        source = self.initial['source']
-        request = self.initial['request']
-        api = OCLapi(request, debug=True)
-        result = api.get('orgs', source['owner'], 'sources', source['id'], 'concepts', concept_id)
-        if result.status_code == 200:
-            raise forms.ValidationError(_('This Concept ID is already used in this source.'))
-        return concept_id
-
-
-class ConceptEditForm(ConceptCreateForm):
-    """
-        Concept edit form
+    Concept edit form
     """
     update_comment = forms.CharField(max_length=90, label=_('Update Comment'), required=False)
 
     def __init__(self, *args, **kwargs):
-        """ Dirty trick to delete one field for edit form. django 1.6 lets you do this
-            officially.
+        """ 
+        Dirty trick to delete one field for edit form. django 1.6 lets you do this officially.
         """
         super(ConceptEditForm, self).__init__(*args, **kwargs)
         self.fields.pop('concept_id')
-        self.fields.pop('name')
         self.fields.pop('locale')
-        self.fields.pop('preferred_locale')
+        self.fields.pop('name')
+        self.fields.pop('name_type')
+        self.fields.pop('description')
+        self.fields.pop('description_type')
 
 
+
+# TODO: Resurrect ConceptNameForm
 class ConceptNameForm(forms.Form):
     """
         Form for a single concept name.
