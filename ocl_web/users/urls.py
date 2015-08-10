7c9acb45efc3472769ed126d176bf33b6d02d936
Jonathan Payne <paynejd@gmail.com>
2015-08-10 10:42:42 -0400

    Removed old apps.sources.urls reference

--- ocl_web/users/urls.py
+++ ocl_web/users/urls.py
@@ -7,8 +7,8 @@ urlpatterns = patterns(
     '',
 
     # TODO(paynejd@gmail.com): Retire this include -- move into main URL config files
-    url(r'^(?P<user>[a-zA-Z0-9\-\.]+)/sources/',
-        include('apps.sources.urls')),
+    #url(r'^(?P<user>[a-zA-Z0-9\-\.]+)/sources/',
+    #    include('apps.sources.urls')),
 
     # URL pattern for the UserListView
     url(regex=r'^$',
