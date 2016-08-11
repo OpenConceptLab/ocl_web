6b79cc660911c46806a1a236600b17c050587ceb
Anshu Aggarwal <anshuag@thoughtworks.com>
2016-08-11 13:06:40 +0530

    +Ravinder #96 added test cases to create a new collection in forms

---
+++ ocl_web/apps/collections/test_forms.py
@@ -0,0 +1,68 @@
+from django.test import TestCase
+from forms import CollectionCreateForm, CollectionEditForm, CollectionVersionAddForm
+from unittest import skip
+
+
+class CollectionCreateTest(TestCase):
+    def test_when_all_valid_data_is_provided_then_new_collection_should_be_made(self):
+        form_data = {
+            'short_name': 'col',
+            'full_name': 'collection',
+            'collection_type': 'Dictionary',
+            'public_access': 'Edit',
+            'default_locale': 'en',
+            'supported_locales': 'en'
+        }
+        form = CollectionCreateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+    def test_when_shortName_is_not_provided_then_form_is_not_valid(self):
+        form_data = {
+            'full_name': 'collection',
+            'collection_type': 'Dictionary',
+            'public_access': 'Edit',
+            'default_locale': 'en',
+            'supported_locales': 'en'
+        }
+        form = CollectionCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_when_FullName_is_not_provided_then_form_is_not_valid(self):
+        form_data = {
+            'short_name': 'col',
+            'collection_type': 'Dictionary',
+            'public_access': 'Edit',
+            'default_locale': 'en',
+            'supported_locales': 'en'
+        }
+        form = CollectionCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_when_defaultLocale_is_not_provided_then_form_is_not_valid(self):
+        form_data = {
+            'short_name': 'col',
+            'full_name': 'collection',
+            'collection_type': 'Dictionary',
+            'public_access': 'Edit',
+            'supported_locales': 'en'
+        }
+        form = CollectionCreateForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+    def test_when_defaultLocales_is_not_provided_then_form_is_not_valid(self):
+        form_data = {
+            'short_name': 'col',
+            'full_name': 'collection',
+            'collection_type': 'Dictionary',
+            'public_access': 'Edit',
+            'default_locale': 'en',
+            'supported_locales': 'en'
+        }
+        form = CollectionCreateForm(data=form_data)
+        self.assertTrue(form.is_valid())
+
+
+
+
+
+
