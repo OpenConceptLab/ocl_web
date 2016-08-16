d79f19b636a496b656019aae4770be860cc6d987
Sny <sny.aggarwal@gmail.com>
2016-08-16 15:57:40 +0530

    +Rohit | 174 | run_server script for deployment

--- run_server.sh
+++ run_server.sh
@@ -1,9 +1,9 @@
 #!/bin/bash -e
 
 cd ocl_web
-source ocl/bin/activate 
-pip install -r requirements.txt 
+source ocl/bin/activate
+pip install -r requirements.txt
 export OCL_API_HOST='localhost:8000'
-export OCL_API_TOKEN='showcase-token'
-export OCL_ANON_API_TOKEN='showcase-token'
+export OCL_API_TOKEN=$1
+export OCL_ANON_API_TOKEN=$1
 nohup grunt serve &
