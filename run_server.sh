afc4a21fbe487cfced33c13cd198166bbd0a48c4
Sny <sny.aggarwal@gmail.com>
2016-08-16 16:59:50 +0530

    +Rohit | 174 | fixing api host url

--- run_server.sh
+++ run_server.sh
@@ -5,7 +5,7 @@ virtualenv ocl
 source ocl/bin/activate
 pip install -r requirements/local.txt
 npm install
-export OCL_API_HOST='localhost:8000'
+export OCL_API_HOST='http://localhost:8000'
 export OCL_API_TOKEN=$1
 export OCL_ANON_API_TOKEN=$1
 nohup grunt serve > /dev/null 2>&1 &
