54545635087dd3592de90d80985494f2d30d5ec2
Sny <sny.aggarwal@gmail.com>
2016-08-16 14:54:55 +0530

    +Rohit | 174 | showcase token hardcoded

--- run_server.sh
+++ run_server.sh
@@ -1,6 +1,9 @@
 #!/bin/bash -e
 
-cd ocl_web 
+cd ocl_web
 source ocl/bin/activate 
 pip install -r requirements.txt 
-grunt serve &
+export OCL_API_HOST='localhost:8000'
+export OCL_API_TOKEN='showcase-token'
+export OCL_ANON_API_TOKEN='showcase-token'
+nohup grunt serve &
