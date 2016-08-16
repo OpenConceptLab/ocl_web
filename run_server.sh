180647b1b1e88b3d336fcb619a3f3e6ddb1040e1
Sny <sny.aggarwal@gmail.com>
2016-08-16 16:24:59 +0530

    +Rohit | 174 | npm install in run script

--- run_server.sh
+++ run_server.sh
@@ -1,9 +1,10 @@
-#!/bin/bash -e
+#!/bin/bash
 
 cd ocl_web
 virtualenv ocl
 source ocl/bin/activate
 pip install -r requirements.txt
+npm install
 export OCL_API_HOST='localhost:8000'
 export OCL_API_TOKEN=$1
 export OCL_ANON_API_TOKEN=$1
