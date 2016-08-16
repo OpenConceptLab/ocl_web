f63f9c3d101be66fc96dc3662e7ca0bd9006a7a9
Sny <sny.aggarwal@gmail.com>
2016-08-16 16:43:40 +0530

    +Rohit | 174 | running local requirements

--- run_server.sh
+++ run_server.sh
@@ -3,9 +3,10 @@
 cd ocl_web
 virtualenv ocl
 source ocl/bin/activate
-pip install -r requirements.txt
+pip install -r requirements/local.txt
 npm install
 export OCL_API_HOST='localhost:8000'
 export OCL_API_TOKEN=$1
 export OCL_ANON_API_TOKEN=$1
+pkill -9 -f
 nohup grunt serve > /dev/null 2>&1 &;
