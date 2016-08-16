a073ece1e3df406f55061324584801101b89bad5
Rohit Garg <rohitgarg19@gmail.com>
2016-08-16 17:51:24 +0530

    +Sny | 174 | Added settings for showcase

--- ocl_web/config/settings.py
+++ ocl_web/config/settings.py
@@ -351,6 +351,26 @@ class Local(Common):
 
     ########## Your local stuff: Below this line define 3rd party libary settings
 
+class Showcase(Common):
+    """ Local class """
+
+    ########## INSTALLED_APPS
+    INSTALLED_APPS = Common.INSTALLED_APPS
+    INSTALLED_APPS += ('django_extensions',)
+    ########## END INSTALLED_APPS
+
+    ########## django-debug-toolbar
+    MIDDLEWARE_CLASSES = Common.MIDDLEWARE_CLASSES + ('debug_toolbar.middleware.DebugToolbarMiddleware',)
+    INSTALLED_APPS += ('debug_toolbar',)
+
+    INTERNAL_IPS = ('127.0.0.1',)
+
+    DEBUG_TOOLBAR_CONFIG = {
+        'INTERCEPT_REDIRECTS': False,
+        'SHOW_TEMPLATE_CONTEXT': True,
+    }
+    ########## end django-debug-toolbar
+
 
 class Production(Common):
     """ Production class -- the default """
