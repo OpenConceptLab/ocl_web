1e26b48e6649495628be1f126d8d0e55dddb173a
Anjali Wadhwa <anju.4321@gmail.com>
2016-08-17 12:20:29 +0530

    +Sny | changing port to 80

--- ocl_web/tests/ui_tests/home_spec.js
+++ ocl_web/tests/ui_tests/home_spec.js
@@ -1,6 +1,6 @@
 describe('OCL Home Page', function() {
   it('should have a title', function() {
-    browser.get('http://localhost:7000');
+    browser.get('http://localhost:80');
 
     expect(browser.getTitle()).toEqual('Open Concept Lab');
   });
