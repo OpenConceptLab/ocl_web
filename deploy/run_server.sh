df2a4a6377cbbffd8847115cdbcde6b80d40fc06
Sny <sny.aggarwal@gmail.com>
2016-08-22 15:32:14 +0530

    +Bhawna | 178 | making run_server generic

--- deploy/run_server.sh
+++ deploy/run_server.sh
@@ -3,9 +3,9 @@
 cd ocl_web
 virtualenv ocl
 source ocl/bin/activate
-pip install -r requirements/local.txt
+pip install -r requirements/$1.txt
 npm install
 export OCL_API_HOST='http://localhost:8000'
-export OCL_API_TOKEN=$1
-export OCL_ANON_API_TOKEN=$1
+export OCL_API_TOKEN=$2
+export OCL_ANON_API_TOKEN=$2
 nohup grunt serve > nohup.out 2>&1 &
