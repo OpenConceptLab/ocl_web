38fc745ebfbe2033ead393d0feb084ff5735c819
Sny <sny.aggarwal@gmail.com>
2016-08-22 15:38:09 +0530

    +Bhawna | 178 | default secret_value

--- ocl_web/manage.py
+++ ocl_web/manage.py
@@ -8,6 +8,7 @@ import sys
 if __name__ == "__main__":
     os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
     os.environ.setdefault("DJANGO_CONFIGURATION", "Local")
+    os.environ.setdefault("DJANGO_SECRET_KEY", "secret")
 
     from configurations.management import execute_from_command_line
 
