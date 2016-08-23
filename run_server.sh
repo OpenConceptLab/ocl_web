83f7b3e508bb46186a494878495ebef5ee3519f5
Sny <sny.aggarwal@gmail.com>
2016-08-23 09:20:36 +0530

    178 | copy the script outside deploy | wip

---
+++ run_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+cd ocl_web
+virtualenv ocl
+source ocl/bin/activate
+pip install -r requirements/$1.txt
+npm install
+export OCL_API_HOST='http://localhost:8000'
+export OCL_API_TOKEN=$2
+export OCL_ANON_API_TOKEN=$2
+nohup grunt serve > nohup.out 2>&1 &
