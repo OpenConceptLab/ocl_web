7e1b579b4ec6042f6736906b642f836301734325
Anjali Wadhwa <anju.4321@gmail.com>
2016-08-23 16:52:21 +0530

    Added e2e test to create, edit and delete a collection

--- ocl_web/tests/ui_tests/conf.js
+++ ocl_web/tests/ui_tests/conf.js
@@ -1,7 +1,7 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:9515',
-    specs: ['home_spec.js'],
+    specs: ['home_spec.js','collections_spec.js'],
     capabilities: {
         'browserName': 'phantomjs'
     }
