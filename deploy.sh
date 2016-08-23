e8bc1104ffb0d992f268f92c69f6fd0cbbf9bfd1
Sny <sny.aggarwal@gmail.com>
2016-08-23 12:05:48 +0530

    178 | deploy script with IP externalized

--- deploy.sh
+++ deploy.sh
@@ -8,4 +8,4 @@ if ssh root@$IP "pkill -9 -f grunt"; then echo "killed grunt"; fi;
 if ssh root@$IP "pkill -9 -f ocl_web"; then echo "killed ocl_web"; fi;
 ssh root@$IP 'rm -rf ocl_web'
 ssh root@$IP "tar -xzf releases/ocl_web$RELEASE_VERSION.tgz && mv repo ocl_web"
-ssh root@$IP "DJANGO_CONFIGURATION=$ENV OCL_WEB_SOCKET_ADDRESS=0.0.0.0:80 ./ocl_web/run_server.sh $SETTINGS $TOKEN
+ssh root@$IP "DJANGO_CONFIGURATION=$ENV OCL_WEB_SOCKET_ADDRESS=0.0.0.0:80 ./ocl_web/run_server.sh $SETTINGS $TOKEN"
