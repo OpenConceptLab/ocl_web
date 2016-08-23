d5c8db8da90a3f2452d6f2a6e79e0b279aa981c0
Anjali Wadhwa <anju.4321@gmail.com>
2016-08-24 00:31:24 +0530

    Resolved merge conflict

--- ocl_web/tests/ui_tests/conf.js
+++ ocl_web/tests/ui_tests/conf.js
@@ -1,11 +1,7 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:9515',
-<<<<<<< HEAD
     specs: ['../ui_tests/specs/*.js'],
-=======
-    specs: ['home_spec.js','collections_spec.js'],
->>>>>>> 6ccf9866724af8a402675f84b2bc4aa7417a2460
     capabilities: {
         'browserName': 'phantomjs'
     },
