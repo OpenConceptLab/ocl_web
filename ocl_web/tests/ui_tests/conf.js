8b718a324eb6302cc51e828574f6d9c3549c3789
Anjali Wadhwa <anju.4321@gmail.com>
2016-08-24 23:45:40 +0530

    Added e2e tests for source and concept creation under org

--- ocl_web/tests/ui_tests/conf.js
+++ ocl_web/tests/ui_tests/conf.js
@@ -2,7 +2,7 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:9515',
     // seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['./specs/collections_spec.js'],
+    specs: ['./specs/collections_spec.js','./specs/organization_spec.js'],
     capabilities: {
         'browserName': 'phantomjs'
         // 'browserName': 'chrome'
--- ocl_web/tests/ui_tests/specs/organization_spec.js
+++ ocl_web/tests/ui_tests/specs/organization_spec.js
@@ -1,3 +1,7 @@
+'use strict';
+
+var LoginPage = require('../pages/login_page.js');
+
 describe('OCL Org Page', function () {
 
     it('should login', function () {
@@ -5,28 +9,61 @@ describe('OCL Org Page', function () {
         loginPage.visit();
         loginPage.login('awadhwa','root123');
 
-        expect(element(by.linkText('awadhwa')).getText()).toEqual('awadhwa');
+        expect(element(by.className('alert-success')).getText()).toEqual('Successfully signed in as awadhwa.');
     });
+
+    var getRandomString = function(length) {
+        var string = '';
+        var letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+        for (var i = 0; i < length; i++) {
+            string += letters.charAt(Math.floor(Math.random() * letters.length));
+        }
+        return string;
+    };
 
     it('should create organization', function () {
         element(by.linkText('Create New Organization')).click();
-        element(by.id('id_short_name')).sendKeys('testOrg2');
-        element(by.id('id_name')).sendKeys('testOrg2');
-        element(by.id('id_website')).sendKeys('http://testOrg2.com');
-        element(by.id('id_company')).sendKeys('testOrg2');
-        element(by.id('id_location')).sendKeys('Geneva');
+        element(by.id('id_short_name')).sendKeys(getRandomString(5));
+        element(by.id('id_name')).sendKeys('Full name');
+        element(by.id('id_website')).sendKeys('http://www.google.com/');
+        element(by.id('id_company')).sendKeys('TW');
+        element(by.id('id_location')).sendKeys('India');
         element(by.buttonText('Create Organization')).click();
 
         expect(element(by.className('alert-info')).getText()).toEqual('Organization Added');
-    })
+    });
 
-    it('should edit organization', function () {
-        element(by.css('span.glyphicon-cog')).click();
-        element(by.id('id_name')).sendKeys('Edited');
-        element(by.buttonText('Update')).click();
+    it('should create source', function () {
+        element(by.linkText('Sources')).click();
+        element(by.linkText(' New Source')).click();
+        element(by.id('id_short_name')).sendKeys('S1');
+        element(by.id('id_full_name')).sendKeys('source 1');
+        element(by.id('id_supported_locales')).sendKeys('en');
+        element(by.buttonText('Create Source')).click();
 
-        expect(element(by.className('alert-info')).getText()).toEqual('Organization updated.');
-    })
+        expect(element(by.className('alert-info')).getText()).toEqual('Source created');
+    });
+
+    // it('should create source version', function () {
+    //     element(by.linkText('Versions')).click();
+    //     element(by.linkText(' New Source Version')).click();
+    //     element(by.id('id_id')).sendKeys('v1');
+    //     element(by.id('id_description')).sendKeys('version 1');
+    //     element(by.buttonText('Create Source Version')).click();
+    //
+    //     expect(element(by.className('alert-info')).getText()).toEqual('Source version created!');
+    // });
+
+    it('should create concept', function () {
+        element(by.linkText('Concepts')).click();
+        element(by.linkText(' New Concept')).click();
+        element(by.id('id_concept_id')).sendKeys('C1');
+        element(by.id('id_name')).sendKeys('concept 1');
+        element(by.id('id_name_type')).sendKeys('Fully_specified');
+        element(by.buttonText('Create Concept')).click();
+
+        expect(element(by.className('alert-info')).getText()).toEqual('Concept created.');
+    });
 
      it('should logout', function () {
         element(by.linkText('Logout')).click();
