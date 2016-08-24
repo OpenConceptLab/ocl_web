c93b48deb581bf0a9de0eccd4163fa2fdf9592e9
Anshu Aggarwal <anshuag@thoughtworks.com>
2016-08-24 11:24:34 +0530

    +Anjali added id to description and externalId for collection detail page

--- ocl_web/tests/ui_tests/conf.js
+++ ocl_web/tests/ui_tests/conf.js
@@ -1,11 +1,11 @@
 exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:9515',
-    //seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['collections_spec.js'],
+    // seleniumAddress: 'http://localhost:4444/wd/hub',
+    specs: ['./specs/collections_spec.js'],
     capabilities: {
         'browserName': 'phantomjs'
-        //'browserName': 'chrome'
+        // 'browserName': 'chrome'
     },
     baseUrl: 'http://showcase.openconceptlab.org'
 };
