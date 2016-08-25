a2228ff00666d1ec5f00a03a9d5a0f700f6269de
bhawnasingla <bhawnas@thoughtworks>
2016-08-25 16:39:58 +0530

    #100 | +Rohit, Added UI for rendering concepts of a collection, basic one

--- ocl_web/config/users_urls.py
+++ ocl_web/config/users_urls.py
@@ -294,6 +294,9 @@ urlpatterns = patterns(
     # /users/:user/collections/:collection/concepts/
     url(r'^(?P<user>[a-zA-Z0-9\-\.]+)/collections/(?P<collection>[a-zA-Z0-9\-\.]+)/concepts/$',
         CollectionConceptsView.as_view(), name='collection-concepts'),
+    # /users/:user/collections/:collection/:collection_version/concepts/
+    url(r'^(?P<user>[a-zA-Z0-9\-\.]+)/collections/(?P<collection>[a-zA-Z0-9\-\.]+)/(?P<collection_version>[a-zA-Z0-9\-\.]+)/concepts/$',
+        CollectionConceptsView.as_view(), name='collection-concepts'),
     # /users/:user/collections/:collection/mappings/
     url(r'^(?P<user>[a-zA-Z0-9\-\.]+)/collections/(?P<collection>[a-zA-Z0-9\-\.]+)/mappings/$',
         CollectionMappingsView.as_view(), name='collection-mappings'),
