a7efefa7a485ad9d4c013281d8ebb7d7e77a89ec
Anjali Wadhwa <anju.4321@gmail.com>
2016-08-26 17:34:51 +0530

    Added e2e tests for create user source, version and concept

---
+++ ocl_web/tests/ui_tests/pages/user_source_page.js
@@ -0,0 +1,38 @@
+var UserSourcePage = function() {
+
+    // user source locators
+    this.newUserSrclink = element(by.linkText('New User Source'));
+    this.shortCode = $('#id_short_name');
+    this.srcFullName = $('#id_full_name');
+    this.supportedLocale = $('#id_supported_locales');
+    this.createUsrSourceButton = element(by.buttonText('Create Source'));
+
+    this.clickNewUsrSrcLink = function () {
+        this.newUserSrclink.click();
+    };
+
+    this.setShortCode = function (code) {
+        this.shortCode.sendKeys(code);
+    };
+
+    this.setSrcFullName = function (full_name) {
+        this.srcFullName.sendKeys(full_name);
+    };
+
+    this.setSupportedLocale = function (locale) {
+       this.supportedLocale.sendKeys(locale)
+    };
+
+    this.clickCreateUsrSrcButton = function () {
+       this.createUsrSourceButton.click();
+    };
+
+    this.createNewUsrSource = function (code, full_name, locale){
+        this.clickNewUsrSrcLink();
+        this.setShortCode(code);
+        this.setSrcFullName(full_name);
+        this.setSupportedLocale(locale);
+        this.clickCreateUsrSrcButton();
+    };
+};
+module.exports = UserSourcePage;
