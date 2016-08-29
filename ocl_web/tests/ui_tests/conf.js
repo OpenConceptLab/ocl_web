0840e7c9ad1181105fa7a0573d41ac73b5dd6be3
Anjali Wadhwa <anju.4321@gmail.com>
2016-08-29 19:03:23 +0530

    E2E test for Add reference for a concept in collection

--- ocl_web/tests/ui_tests/conf.js
+++ ocl_web/tests/ui_tests/conf.js
@@ -2,7 +2,7 @@ exports.config = {
     framework: 'jasmine',
     seleniumAddress: 'http://localhost:9515',
     // seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['./specs/collections_spec.js','./specs/organization_spec.js', './specs/user_source_spec.js'],
+    specs: ['./specs/organization_spec.js','./specs/user_source_spec.js','./specs/collections_spec.js'],
     capabilities: {
         'browserName': 'phantomjs'
         // 'browserName': 'chrome'
