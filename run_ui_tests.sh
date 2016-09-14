42d69e98e7bcb940001621d398e6861aad4dd344
Anjali Wadhwa <anju.4321@gmail.com>
2016-09-14 19:51:48 +0530

    testing Xvfb

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -6,4 +6,3 @@ export DISPLAY=:99
 nohup ./node_modules/protractor/bin/webdriver-manager start > nohup_ui_tests.out  2>&1 &
 sleep 3
 ./node_modules/protractor/bin/protractor ./ocl_web/tests/ui_tests/conf.js
-kill %1
