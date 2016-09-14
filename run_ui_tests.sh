6da2da391d2e08175fa241aa0321163344dfe3bf
Anjali Wadhwa <anju.4321@gmail.com>
2016-09-14 17:09:14 +0530

    using xvfb for e2e tests

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
 cd ~/ocl_web
-nohup ./node_modules/protractor/bin/webdriver-manager start > nohup_ui_tests.out  2>&1 &
-sleep 3
 ./node_modules/protractor/bin/protractor ./ocl_web/tests/ui_tests/conf.js
