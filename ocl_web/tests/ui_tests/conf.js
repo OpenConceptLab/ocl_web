2c7fcd4f6e0285423b8d6691344691fac591d509
Anjali Wadhwa <anju.4321@gmail.com>
2016-09-14 17:57:54 +0530

    removed selenium address

--- ocl_web/tests/ui_tests/conf.js
+++ ocl_web/tests/ui_tests/conf.js
@@ -1,7 +1,7 @@
 exports.config = {
     framework: 'jasmine',
     // seleniumAddress: 'http://localhost:9515',
-    seleniumAddress: 'http://localhost:4444/wd/hub',
+    // seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['./specs/organization_spec.js','./specs/user_source_spec.js','./specs/collections_spec.js'],
     capabilities: {
         // 'browserName': 'phantomjs'
