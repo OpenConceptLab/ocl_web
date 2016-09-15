11b343af777bdc3430f91838deb10f415f9be3b8
Anjali Wadhwa <anju.4321@gmail.com>
2016-09-15 22:17:29 +0530

    Added create version, release/un-release a version and add reference tests for user collection

--- ocl_web/tests/ui_tests/specs/collections_spec.js
+++ ocl_web/tests/ui_tests/specs/collections_spec.js
@@ -39,19 +39,80 @@ describe('OCL Collections Page', function () {
         expect((collectionPage.status).getText()).toEqual('Collection created');
     });
 
-    it('should edit collection', function () {
-        collectionPage.editCollection(data.col_desc, data.ext_id);
+    it('should create collection version', function () {
+       element(by.linkText('Versions')).click();
+       element(by.linkText('New Collection Version')).click();
+       $('#id_id').sendKeys('V1');
+       $('#id_description').sendKeys('version 1');
+       element(by.buttonText('Create Collection Version')).click();
 
-        expect((collectionPage.status).getText()).toEqual('Collection updated');
-        expect((collectionPage.updatedDescValue).getText()).toContain('collection description');
-        expect((collectionPage.updatedExtIdValue).getText()).toContain('1.1');
+       expect((orgPage.status).getText()).toEqual('Collection version created!');
     });
 
-    it('should delete collection', function () {
-        collectionPage.deleteCollection();
+    it('should release a user collection version', function () {
+        orgPage.releaseVersion();
+        browser.sleep('500');
 
-        expect((collectionPage.status).getText()).toEqual('Collection Deleted');
+        expect(orgPage.releaseLabel.getText()).toEqual('Released');
+        expect(orgPage.message.getText()).toEqual('Successfully Released.');
+
+        browser.sleep('500');
+        orgPage.message.click();
+        browser.sleep('750');
     });
+
+    it('should un-release a user collection version', function () {
+        orgPage.releaseVersion();
+        browser.sleep('500');
+
+        expect(orgPage.message.getText()).toEqual('Successfully Un-Released.');
+
+        browser.sleep('500');
+        orgPage.message.click();
+        // browser.sleep('500');
+    });
+
+    it('should add a reference of concept to a collection', function () {
+        var concept_expression = '/orgs/EthiopiaMOH/sources/HSTP-Indicators/concepts/1/';
+        orgPage.createNewReference(concept_expression);
+        // expect((orgPage.status).getText()).toEqual('Expression added.');
+        // expect(element(by.linkText(' '+expression)).isPresent()).toBe(true);
+    });
+
+    // it('should delete without selecting reference', function () {
+    //     $('.delete-reference').click();
+    //     browser.sleep('500');
+    //
+    //     expect(orgPage.warning.getText()).toEqual('Please select references!');
+    //
+    //     // browser.sleep('500');
+    //     orgPage.warning.click();
+    //     browser.sleep('500');
+    // });
+    //
+    // it('should delete a reference of concept from org collection', function () {
+    //     orgPage.deleteReference();
+    //
+    //     expect(orgPage.message.getText()).toEqual('Successfully removed.');
+    //
+    //     browser.sleep('500');
+    //     orgPage.message.click();
+    //     browser.sleep('500');
+    // });
+
+    // it('should edit collection', function () {
+    //     collectionPage.editCollection(data.col_desc, data.ext_id);
+    //
+    //     expect((collectionPage.status).getText()).toEqual('Collection updated');
+    //     expect((collectionPage.updatedDescValue).getText()).toContain('collection description');
+    //     expect((collectionPage.updatedExtIdValue).getText()).toContain('1.1');
+    // });
+
+    // it('should delete collection', function () {
+    //     collectionPage.deleteCollection();
+    //
+    //     expect((collectionPage.status).getText()).toEqual('Collection Deleted');
+    // });
 
      it('should logout', function () {
          logoutPage.logout();
