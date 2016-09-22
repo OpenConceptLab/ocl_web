586ebf296e53e9d28c213293f5648bf63a651ea6
Anjali Wadhwa <anju.4321@gmail.com>
2016-09-22 19:57:50 +0530

    updated the release label locator

--- ocl_web/tests/ui_tests/pages/organization_page.js
+++ ocl_web/tests/ui_tests/pages/organization_page.js
@@ -39,7 +39,7 @@ var OrganizationPage = function() {
     // Release / Un-release version locators
     this.releaseButton = $('#id_release');
     this.message = $('.ajs-success');
-    this.releaseLabel = $('.release-label');
+    this.releaseLabel = $('#released');
 
     // Delete reference locators
     this.deleteLink = $('.delete-reference');
