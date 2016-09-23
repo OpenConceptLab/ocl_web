e345637cf82dee2ef3ec6899b0cb0c25f45b417f
Anjali Wadhwa <anju.4321@gmail.com>
2016-09-23 16:50:29 +0530

    used class instread of id in e2e test

--- ocl_web/tests/ui_tests/pages/organization_page.js
+++ ocl_web/tests/ui_tests/pages/organization_page.js
@@ -39,7 +39,7 @@ var OrganizationPage = function() {
     // Release / Un-release version locators
     this.releaseButton = $('#id_release');
     this.message = $('.ajs-success');
-    this.releaseLabel = $$('#id_released');
+    this.releaseLabel = $$('.release-label');
 
     // Delete reference locators
     this.deleteLink = $('.delete-reference');
