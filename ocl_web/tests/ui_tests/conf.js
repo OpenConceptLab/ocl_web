abb90f6df247858fc9856f7339681ea9f05948b9
Anjali Wadhwa <anju.4321@gmail.com>
2016-10-04 22:11:16 +0530

    Updated config file for e2e tests

--- ocl_web/tests/ui_tests/conf.js
+++ ocl_web/tests/ui_tests/conf.js
@@ -1,13 +1,19 @@
 exports.config = {
     framework: 'jasmine',
+
+    // this is config to run in headless mode
     seleniumAddress: 'http://localhost:9515',
-    // seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['./specs/collections_spec.js','./specs/organization_spec.js','./specs/user_source_spec.js'],
     capabilities: {
         'browserName': 'phantomjs'
-        // 'browserName': 'chrome'
-
     },
+
+    // Uncomment below 3 lines and comment headless mode config above to run in Chrome browser
+    //     seleniumAddress: 'http://localhost:4444/wd/hub',
+    //     capabilities: {
+    //         'browserName': 'chrome'
+    //     },
+    specs: ['./specs/organization_spec.js','./specs/user_source_spec.js','./specs/collections_spec.js'],
+
     'onPrepare' : function () {
         browser.driver.manage().window().setSize(1280, 1024);
     },
