5b20b7933eaf4c6d5f847e240567d3f08e2a1789
Anjali Wadhwa <anju.4321@gmail.com>
2016-10-05 17:30:24 +0530

    Removed commented code

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 cd ~/ocl_web
-# Xvfb :99 &
-# export DISPLAY=:99
-# nohup ./node_modules/protractor/bin/webdriver-manager start > nohup_ui_tests.out  2>&1 &
 nohup phantomjs --webdriver=9515 > nohup_ui_tests.out  2>&1 &
 sleep 3
 ./node_modules/protractor/bin/protractor ./ocl_web/tests/ui_tests/conf.js
