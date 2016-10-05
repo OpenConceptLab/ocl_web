eeff0bb81671531af6e645006f4f53227c3c43a4
Anjali Wadhwa <anju.4321@gmail.com>
2016-10-06 02:28:48 +0530

    Revert "Updated phantomjs port"
    This reverts commit 578c7504c29d744b77c3f3b37c67ea55c67acbb3.

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 cd ~/ocl_web
-nohup phantomjs --webdriver=4444 > nohup_ui_tests.out  2>&1 &
-sleep 3
+#nohup phantomjs --webdriver=9515 > nohup_ui_tests.out  2>&1 &
+#sleep 3
 ./node_modules/protractor/bin/protractor ./ocl_web/tests/ui_tests/conf.js
