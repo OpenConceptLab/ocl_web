cf94e44845d8a4ed6a26da51051ee7e14a525fa1
Anjali Wadhwa <anju.4321@gmail.com>
2016-10-06 12:19:11 +0530

    Fixed run tests script

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 cd ~/ocl_web
+nohup ./node_modules/protractor/bin/webdriver-manager update > nohup_ui_tests.out  2>&1 &
 #nohup phantomjs --webdriver=9515 > nohup_ui_tests.out  2>&1 &
-#sleep 3
+sleep 3
 ./node_modules/protractor/bin/protractor ./ocl_web/tests/ui_tests/conf.js
