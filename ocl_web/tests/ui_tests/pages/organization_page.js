536a96c007e32a3798e568d8dd8829362ef8b5b0
Anjali Wadhwa <anju.4321@gmail.com>
2016-10-06 12:49:51 +0530

    Small fix

--- ocl_web/tests/ui_tests/pages/organization_page.js
+++ ocl_web/tests/ui_tests/pages/organization_page.js
@@ -108,7 +108,7 @@ var OrganizationPage = function() {
         this.addNewReferenceLink.click();
         this.expression.sendKeys(expression);
         this.addReferenceButton.click();
-        browser.sleep('500');
+        browser.sleep('1000');
     };
 
     this.deleteReference = function () {
