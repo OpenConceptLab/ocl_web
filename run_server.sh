80bc45d7b81e0cb2e5b71c898ea80e05d28aad21
Gaurav Butola <gbutola@thoughtworks.com>
2016-10-07 15:26:38 +0530

    Use get_user_model to get the overriden User model

--- run_server.sh
+++ run_server.sh
@@ -9,4 +9,4 @@ export OCL_API_HOST='http://localhost:8000'
 export OCL_API_TOKEN=$2
 export OCL_ANON_API_TOKEN=$2
 nohup grunt serve > nohup.out 2>&1 &
-echo "from django.contrib.auth.models import User; user = User.objects.create_superuser('admin', 'paynejd@gmail.com', 'Root123') if (User.objects.filter(username='admin').count() < 1) else None" | python ocl_web/manage.py shell
+echo "from django.contrib.auth import get_user_model; User = get_user_model(); user = User.objects.create_superuser('admin', 'paynejd@gmail.com', 'Root123') if (User.objects.filter(username='admin').count() < 1) else None" | python ocl_web/manage.py shell
