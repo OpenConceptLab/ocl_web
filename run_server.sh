d795faf150d95e965a3cb421a62128d810c21734
Gaurav Butola <gbutola@thoughtworks.com>
2016-10-07 15:03:36 +0530

    Fix admin user creation

--- run_server.sh
+++ run_server.sh
@@ -9,4 +9,4 @@ export OCL_API_HOST='http://localhost:8000'
 export OCL_API_TOKEN=$2
 export OCL_ANON_API_TOKEN=$2
 nohup grunt serve > nohup.out 2>&1 &
-echo "from django.contrib.auth.models import User; user = User.objects.create_superuser('admin', 'paynejd@gmail.com', 'Root123') if (User.objects.filter(username='root').count() < 1) else None" | python manage.py shell
+echo "from django.contrib.auth.models import User; user = User.objects.create_superuser('admin', 'paynejd@gmail.com', 'Root123') if (User.objects.filter(username='admin').count() < 1) else None" | python manage.py shell
