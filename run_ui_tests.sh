38908ed670fc1e9c18bd6dbc03a86b53f6243e83
Anjali Wadhwa <anju.4321@gmail.com>
2016-10-17 22:05:29 +0530

    Added selenium address and code to start server before the test

--- run_ui_tests.sh
+++ run_ui_tests.sh
@@ -11,4 +11,9 @@ rm -rf ./node_modules/protractor/node_modules/webdriver-manager/selenium
 echo "Updating web driver manager"
 nohup ./node_modules/protractor/bin/webdriver-manager update
 
+echo "Starting server"
+nohup  ./node_modules/protractor/bin/webdriver-manager start > nohup.out 2>&1 &
+
+sleep 3
+
 ./node_modules/protractor/bin/protractor ./ocl_web/tests/ui_tests/conf.js --verbose
