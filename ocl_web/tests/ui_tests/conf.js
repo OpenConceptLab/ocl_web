ed33adf77581b2fe698b33ab27cd73b25a4eef54
Anjali Wadhwa <anju.4321@gmail.com>
2016-10-17 22:26:35 +0530

    Removing home_page spec test as it is not required

--- ocl_web/tests/ui_tests/conf.js
+++ ocl_web/tests/ui_tests/conf.js
@@ -28,7 +28,7 @@
     exports.config = {
         framework: 'jasmine',
         seleniumAddress: 'http://localhost:4444/wd/hub',
-        specs: ['./specs/home_spec.js', './specs/organization_spec.js','./specs/user_source_spec.js','./specs/collections_spec.js','./specs/user_source_validation_spec.js'],
+        specs: ['./specs/organization_spec.js','./specs/user_source_spec.js','./specs/collections_spec.js','./specs/user_source_validation_spec.js'],
         capabilities: getBrowserCapabilities(),
 
         'onPrepare' : function () {
