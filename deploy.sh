5e7895ec108a6687c2dd3bd7869911944cc9ed85
Burak Ince <bince@thoughtworks.com>
2017-01-31 14:41:47 +0300

    #418 Fix shell script command execute problem

--- deploy.sh
+++ deploy.sh
@@ -8,7 +8,7 @@ if ssh root@$IP "pkill -9 -f grunt"; then echo "killed grunt"; fi;
 if ssh root@$IP "pkill -9 -f ocl_web"; then echo "killed ocl_web"; fi;
 ssh root@$IP 'rm -rf ocl_web'
 ssh root@$IP "tar -xzf releases/ocl_web$RELEASE_VERSION.tgz && mv repo ocl_web"
-if ssh root@$IP "~/ocl_web/deploy/manage_releases.sh clear_releases"; then echo "Removed old releases"; fi;
+if ssh root@$IP "source ~/ocl_web/deploy/manage_releases.sh && clear_releases ~/releases/"; then echo "Removed old releases"; fi;
 if [[ -z $PORT ]]
 then
 	PORT=80
