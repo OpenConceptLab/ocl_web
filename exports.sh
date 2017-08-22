73837afa18a06786a3886add5dd247b0bb041207
davetrig <davetrig@yahoo.com>
2017-08-22 14:42:12 -0400

    Delete exports.sh
    Included in commit by mistake

--- exports.sh
+++
@@ -1,4 +0,0 @@
-export OCL_API_HOST='http://0.0.0.0:8000'
-export OCL_API_TOKEN='13fae5abbe4c678e4e25e4ae3c05d85943368454'
-export OCL_ANON_API_TOKEN='13fae5abbe4c678e4e25e4ae3c05d85943368454'
-export DATABASE_URL=sqlite://///home/davetrig/ocl_web/ocl.db
